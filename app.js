47a4ede3f78f4e18ccc9dba574567ea38b3f5aca
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-04-22 02:49:28 +0200

    get all possible lines

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
+/* eslint-disable no-underscore-dangle */
 const gameboard =(() =>{
-    const values = Array(9).fill(null)
+    const values = Array(9).fill(null);
+
+    const _rows = function () {
+        const rows=[]
+        for (let i=0;i<9;i+=3) {
+            rows.push(values.slice(i,i+3))
+        }
+        return rows
+    }
+
+    const _columns = function () {
+        const columns=[]
+        for (let i=0;i<=2;i++) {
+            const column=[]
+            for  (let j=0;j<9;j+=3) {
+                column.push(values[i+j])
+            }
+            columns.push(column)
+        }
+        return columns
+    }
     
+    const _diagonals = function () {
+        const diagonals = [[values[0],values[4],values[8]],
+                           [values[2],values[4],values[6]]];
+
+        return diagonals;
+    }
+
+    const getLines = function () {
+        return [..._rows(),..._columns(),..._diagonals()]
+    }
+    return {
+        _rows,
+        _columns,
+        _diagonals,
+        getLines
+    }
 })();
 
 const game =(() =>{
